840a07f1b8f93bc0e8420a6b20bebed8778c841b
daza9604 <90068737+daza9604@users.noreply.github.com>
2023-09-07 01:20:13 -0500

    Delete src directory

--- src/main/java/pe/edu/upc/aaw/demo01/repositories/IConfiguracionRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.aaw.demo01.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.aaw.demo01.entities.Configuracion;
-
-@Repository
-public interface IConfiguracionRepository extends JpaRepository<Configuracion,Integer> {
-}
--- src/main/java/pe/edu/upc/aaw/demo01/servicesinterfaces/IDispositivoConectadoService.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.aaw.demo01.servicesinterfaces;
-
-import pe.edu.upc.aaw.demo01.entities.DispositivoConectado;
-
-import java.util.List;
-
-public interface IDispositivoConectadoService {
-    public List<DispositivoConectado> list();
-}

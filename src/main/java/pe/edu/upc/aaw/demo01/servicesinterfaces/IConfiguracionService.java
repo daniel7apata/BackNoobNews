1b617250fd67b8611f00b0bad3b2e625506bc0e6
daza9604 <90068737+daza9604@users.noreply.github.com>
2023-09-08 03:29:24 -0500

    Delete src directory

--- src/main/java/pe/edu/upc/aaw/demo01/servicesinterfaces/IConfiguracionService.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upc.aaw.demo01.servicesinterfaces;
-
-import pe.edu.upc.aaw.demo01.entities.Configuracion;
-
-public interface IConfiguracionService {
-
-    public void insert(Configuracion configuracion);
-}

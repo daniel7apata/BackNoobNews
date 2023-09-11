d4f50f58fe50c319a79ea173b5359d29a83ec7ab
oscarb19 <85407736+oscarb19@users.noreply.github.com>
2023-09-11 00:11:44 -0500

    Add files via upload

---
+++ src/main/java/pe/edu/upc/aaw/demo01/dtos/UserRoleDTO.java
@@ -0,0 +1,23 @@
+package pe.edu.upc.aaw.demo01.dtos;
+
+public class UserRoleDTO {
+
+    private String description;
+    private int quantityUser;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getQuantityUser() {
+        return quantityUser;
+    }
+
+    public void setQuantityUser(int quantityUser) {
+        this.quantityUser = quantityUser;
+    }
+}

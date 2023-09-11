42342a140148fee169c491daf57f88d0754b38ae
JordanOnofreRuiz <116929470+JordanOnofreRuiz@users.noreply.github.com>
2023-09-11 02:07:51 -0500

    update categorycontroller

--- src/main/java/pe/edu/upc/aaw/demo01/controllers/CategoryController.java
+++ src/main/java/pe/edu/upc/aaw/demo01/controllers/CategoryController.java
@@ -5,10 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.aaw.demo01.dtos.CategoryDTO;
 import pe.edu.upc.aaw.demo01.dtos.ConfigurationDTO;
+import pe.edu.upc.aaw.demo01.dtos.PublicationCategoryDTO;
+import pe.edu.upc.aaw.demo01.dtos.UserUniversityDTO;
 import pe.edu.upc.aaw.demo01.entities.Category;
 import pe.edu.upc.aaw.demo01.entities.Configuration;
 import pe.edu.upc.aaw.demo01.servicesinterfaces.ICategoryService;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -47,11 +50,20 @@ public class CategoryController {
         CategoryDTO dto=m.map(catS.listId(id), CategoryDTO.class);
         return dto;
     }
+    @GetMapping("/publication")
+    public List<PublicationCategoryDTO> getPublicationbyCategory() {
+        List<String[]> countPublicationByCategory = catS.getCountPublicationByCategory();
+        List<PublicationCategoryDTO> publicationCategoryDTOList = new ArrayList<>();
 
-    @PutMapping
-    public void modificar(@RequestBody CategoryDTO dto){
-        ModelMapper m=new ModelMapper();
-        Category c=m.map(dto,Category.class);
-        catS.insert(c);
+        for (String[] data : countPublicationByCategory) {
+            if (data.length >= 2) {
+                PublicationCategoryDTO publicationCategoryDTO = new PublicationCategoryDTO();
+                publicationCategoryDTO.setNameCategory(data[0]);
+                publicationCategoryDTO.setQuantityPublication(Integer.parseInt(data[1]));
+                publicationCategoryDTOList.add(publicationCategoryDTO);
+            }
+        }
+
+        return publicationCategoryDTOList;
     }
 }

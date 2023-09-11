f163094417f7d57ee255caffd999240b40df06d2
daza9604 <90068737+daza9604@users.noreply.github.com>
2023-09-11 01:52:35 -0500

    Update IUniversityRepository.java

--- src/main/java/pe/edu/upc/aaw/demo01/repositories/IUniversityRepository.java
+++ src/main/java/pe/edu/upc/aaw/demo01/repositories/IUniversityRepository.java
@@ -1,6 +1,7 @@
 package pe.edu.upc.aaw.demo01.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.aaw.demo01.entities.University;
 

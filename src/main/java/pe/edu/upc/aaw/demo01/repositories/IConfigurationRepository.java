76c1c750134448963f32734aa64a5e9baa2803df
daza9604 <90068737+daza9604@users.noreply.github.com>
2023-09-14 06:48:50 -0500

    Delete src/main/java/pe/edu/upc/aaw/demo01 directory

--- src/main/java/pe/edu/upc/aaw/demo01/repositories/IConfigurationRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.aaw.demo01.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.aaw.demo01.entities.Configuration;
-
-@Repository
-public interface IConfigurationRepository extends JpaRepository<Configuration,Integer> {
-}

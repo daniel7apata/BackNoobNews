d4b380a01f180464e538feb5787d31d735d0254f
daza9604 <90068737+daza9604@users.noreply.github.com>
2023-09-14 04:09:57 -0500

    Add files via upload

---
+++ src/main/java/pe/edu/upc/aaw/demo01/serviceimplements/IUsersRepository.java
@@ -0,0 +1,20 @@
+package pe.edu.upc.aaw.demo01.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import pe.edu.upc.aaw.demo01.entities.Users;
+
+import java.util.List;
+
+@Repository
+public interface IUsersRepository extends JpaRepository<Users,Integer> {
+  
+@Query(value = "SELECT us.name_user, COUNT(inte.id_interaction) " +
+            "FROM Users us inner join Interaction inte " +
+            "ON us.id_user = inte.id_user " +
+            "GROUP BY us.name_user " +
+            "order BY COUNT(inte.id_interaction) " +
+            "DESC", nativeQuery = true)
+    List<String[]> InteractionUsers();
+}
